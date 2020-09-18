59688989018f177db3d0caf766d9552ccf09bc38
Chris Martin <chris@crunchycloud.io>
2020-09-18 20:52:00 +1000

    move infura project id to env file

--- truffle-config.js
+++ truffle-config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const path = require("path");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const mnemonic = process.env.MNEMONIC
+const infuraProjectIdRopsten = process.env.INFURA_PROJECT_ID_ROPSTEN
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -13,7 +14,7 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/b34b70b4cbc445968e25772ec9fb7212")
+        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraProjectIdRopsten}`)
       },
       network_id: "3",
       gas: 3000000,
